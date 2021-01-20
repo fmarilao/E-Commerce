2fa53f237385ed808fe1b19b02bf74719f4d498e
CreativiTICs <creativiticscolombia@gmail.com>
2021-01-19 20:31:55 -0500

    S 17 in progress
    Defining the logic to update and delete a product category.

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -10,17 +10,48 @@ server.get('/', (req, res, next) => {
 });
 
 //S17
+//Agrega la categoria al producto
 server.post("/:idProduct/category/:idCat", (req, res, next) => {
   let IdCategory = req.params.idCat;
   let idProduct = req.params.idProduct;
+  //Traer antes el nombre de la categoría?
+  Product.update(
+    {
+      //category o childkey??
+      category: Category.name,
+    },
+    {
+      where: {
+        id: idProduct,
+      },
+    }
+  ).then((product) => {
+    res.json(product);
+  });
 });
 
+//Elimina la categoria al producto
 server.delete("/:idProduct/category/:idCat", (req, res, next) => {
   let IdCategory = req.params.idCat;
   let idProduct = req.params.idProduct;
+  //Traer antes el nombre de la categoría?
+  Product.destroy(
+    {
+      //category o childkey??
+      category: Category.name,
+    },
+    {
+      where: {
+        id: idProduct,
+      },
+    }
+  ).then((product) => {
+    res.json(product);
+  });
 });
 
 //S22
+//Crear Ruta que devuelva los productos de X categoria
 server.get("/category/:cat", (req, res, next) => {
   let category = req.params.cat;
   Category.findAll({
