4591d13ae507942e17f08824d8e352a0c17708c9
fmarilao <facundomarilao@gmail.com>
2021-01-20 20:59:31 -0300

    uploading tasks

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,19 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
 
+//Esta ruta ya venia en el repo // VER
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
 			res.send(products);
 		})
 		.catch(next);
+});
+server.get('/products', (req, res) => {
+	Product.findAll()
+		.then(products => {
+			res.send(products);
+		})
 });
 
 module.exports = server;
