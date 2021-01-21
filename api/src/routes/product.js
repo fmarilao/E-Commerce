01dc4e7c0c1537bfcc890ba73fc3024c8d44a98f
fmarilao <facundomarilao@gmail.com>
2021-01-21 11:03:56 -0300

    new changes

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,19 +1,12 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
 
-//Esta ruta ya venia en el repo // VER
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
 			res.send(products);
 		})
 		.catch(next);
-});
-server.get('/products', (req, res) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
 });
 
 module.exports = server;
