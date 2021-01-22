6f85485e34f589ec8d42200ee196e845d016fb20
Zaba <Nzabattaro@gmail.com>
2021-01-22 19:11:55 -0300

    fix little conflicts
    Cors duplicate call
    Fix searchbar allways rendering to exact path '/'

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -3,14 +3,16 @@ import { Route } from 'react-router-dom';
 import Catalog from "../components/catalog/Catalog";
 import SearchBar from '../components/searchbar/SearchBar';
 import ProductDetail from '../components/productDetail/ProductDetail';
+import Dashboard from '../components/admin/dashboard/main/dashboard.jsx'
 
 const routes = () => {
     return (
       <>
-      <Route path="/" component={SearchBar} />
+      <Route exact path="/" component={SearchBar} />
       <Route path="/catalog" component={Catalog} />
       <Route path="/products" component={Catalog} />
       <Route path="/product/:id" component={ProductDetail} />
+      <Route path="/dashboard" component={Dashboard} />
       </>
     );
 }
