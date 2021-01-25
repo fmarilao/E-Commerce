ee48d259496c9169dba923e2d9707e30e6d17bb9
fmarilao <facundomarilao@gmail.com>
2021-01-25 18:32:05 -0300

    Models Order, OrderLine, Users(Carlos) and relationships of those models

---
+++ api/src/models/User.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("user", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dni: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthDate: {
+      type: DataTypes.DATE,
+    },
+    gender: {
+      type: DataTypes.INTEGER,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.INTEGER,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    recoveryId: {
+      type: DataTypes.STRING,
+    },
+  });
+};
