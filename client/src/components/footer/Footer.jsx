28bd425022785e8c37538b2d8135a83af253624f
Jeremias Santochi <jeremiassantochi@gmail.com>
2021-01-29 08:53:28 -0300

    change in footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,31 +1,42 @@
 import React from 'react'
 import ReactWhatsapp from 'react-whatsapp';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+
 
 const Footer = () => {
     return (
-    <div>
       <div>
         <div>
-          {/* Column1 */}
           <div>
-            <h2>ECOMMERCE</h2>
-            <h5>
-              <li>555-555-5555</li>
-              <li>email@email.com</li>
-              <li>Calle Falsa 123</li>
-            </h5>
+            {/* Column1 */}
+            <div>
+              <h2>ECOMMERCE</h2>
+              <h5>
+                <li>555-555-5555</li>
+                <li>email@email.com</li>
+                <li>Calle Falsa 123</li>
+              </h5>
+            </div>
           </div>
-         </div>
-        <hr />
-      <ReactWhatsapp number="1-212-736-5000" message="Chatea con nosotros!!!" />
-        <div>
-          <p>
-            &copy;{new Date().getFullYear()} ECOMMERCE | Todos los derechos reservados | Teminos de servicios | Politica de privacidad
-          </p>
+          <hr />
+
+          <ReactWhatsapp
+            number="1-212-736-5000"
+            message="Chatea con nosotros!!!"
+          >
+            <WhatsAppIcon />
+            Chatea con nosotros
+          </ReactWhatsapp>
+
+          <div>
+            <p>
+              &copy;{new Date().getFullYear()} ECOMMERCE | Todos los derechos
+              reservados | Teminos de servicios | Politica de privacidad
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Footer
