9a077af2c6b9ebff8885c5632dd216b2ff4c46ed
fmarilao <facundomarilao@gmail.com>
2021-01-31 18:59:10 -0300

    pagination

---
+++ client/src/components/pagination/Pagination.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import { Pagination } from '@material-ui/lab';
+import { Grid } from "@material-ui/core";
+
+export default function Pages({totalPages, paginate}) {
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        setPage(1);
+    },[totalPages])
+
+    const handleChange = (event, value) => {
+        setPage(value);
+        paginate(value)
+      };
+      
+    return(
+        <>
+        <Grid item>
+            <Grid item xs={12}>
+              <Pagination count={totalPages} variant="outlined" page={page} onChange={handleChange}/>
+            </Grid>
+        </Grid>
+        </>
+    )
+}
