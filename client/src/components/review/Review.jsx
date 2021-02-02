7b5ac20ef038b2506a3d57083fc9e9d5dfd5e1bc
fmarilao <facundomarilao@gmail.com>
2021-02-02 20:26:47 -0300

    fix error in productDetail

--- client/src/components/review/Review.jsx
+++ client/src/components/review/Review.jsx
@@ -1,24 +1,13 @@
-// import React, { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getReviews, setReview } from '../../redux/reviewsReducer/';
-// import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-// const Review = (props) => {
-//     const dispatch = useDispatch()
-//     const review = useSelector(state => state.reviewsReducer.review)
-//     const reviews = useSelector(state => state.reviewsReducer.reviews)
-//     const product = useSelector(state => state.productsReducer.product)
+const Review = (props) => {
+    
+    return (
+        <div>
+            <h5>Review Component</h5>
+        </div>
+    )
+}
 
-//     useEffect(() => {
-//         dispatch(getReviews(product.idProduct))
-//         return dispatch(setReview(reviews[getRandom(reviews.length)]))
-//     }, [])
-
-//     return (
-//         <div>
-//             <h5>Review Component</h5>
-//         </div>
-//     )
-// }
-
-// export default Review
+export default Review
