0de36f7c03a831dba7ec32e1e2620be42a2537ab
fmarilao <facundomarilao@gmail.com>
2021-02-02 14:46:37 -0300

    structure redux reviews

--- client/src/components/review/Review.jsx
+++ client/src/components/review/Review.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getReviews, setReview } from '../../redux/reviewsReducer/';
+import axios from 'axios';
 
-const Review = () => {
+const Review = (props) => {
+    const dispatch = useDispatch()
+    const review = useSelector(state => state.reviewsReducer.review)
+    const reviews = useSelector(state => state.reviewsReducer.reviews)
+    const product = useSelector(state => state.productsReducer.product)
+
+    useEffect(() => {
+        dispatch(getReviews(product.idProduct))
+        return dispatch(setReview(reviews[getRandom(reviews.length)]))
+    }, [])
+
     return (
         <div>
             <h5>Review Component</h5>
