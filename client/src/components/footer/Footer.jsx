62dce0b33dd289364106b249762de53b6104731f
fmarilao <facundomarilao@gmail.com>
2021-02-01 21:26:22 -0300

    new changes in footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactWhatsapp from 'react-whatsapp';
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
-import { Grid } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { positions } from '@material-ui/system';
 
 const useStyles = makeStyles((theme) => ({
     footer: {
       marginTop: theme.spacing(3),
-      bottom: 0
+      bottom: 0,
     }
 }))
 
@@ -15,26 +16,17 @@ const useStyles = makeStyles((theme) => ({
 const Footer = () => {
   const classes = useStyles();
     return (
-      <Grid container className={classes.footer}
-       direction="column"
-              alignItems="center"
-              justify="center"
-      >
-      <div>
-        <div>
-          <div>
+      <>
+      <Box  width={1} flexDirection="column">
             {/* Column1 */}
             <div>
-              <h2>ECOMMERCE</h2>
               <h5>
                 <li>555-555-5555</li>
                 <li>email@email.com</li>
                 <li>Calle Falsa 123</li>
               </h5>
             </div>
-          </div>
           <hr />
-
           {/* <ReactWhatsapp
             number="1-212-736-5000"
             message="Chatea con nosotros!!!"
@@ -42,15 +34,12 @@ const Footer = () => {
             <WhatsAppIcon />
             Chatea con nosotros
           </ReactWhatsapp> */}
-          <div>
-            <p>
+      </Box>
+      <Box display="flex" justifyContent="center">
               &copy;{new Date().getFullYear()} ECOMMERCE | Todos los derechos
               reservados | Teminos de servicios | Politica de privacidad
-            </p>
-          </div>
-        </div>
-      </div>
-      </Grid>
+      </Box>
+    </>
     );
 }
 
