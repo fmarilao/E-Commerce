d4b7bde3cbe578668b40f205a4dcf37e041d5145
Jeremias Santochi <jeremiassantochi@gmail.com>
2021-02-03 11:01:01 -0300

    post a review Action and Reducer

--- client/src/redux/reviewsReducer/actionsReviews.js
+++ client/src/redux/reviewsReducer/actionsReviews.js
@@ -1,38 +1,41 @@
 import axios from "axios";
-
 export const POST_REVIEW = 'POST_REVIEW'
-export const SET_REVIEW = 'SET_REVIEW'
-export const EDIT_REVIEW = 'EDIT_REVIEW'
-export const GET_REVIEWS = 'GET_REVIEWS'
-export const DELETE_REVIEW = 'DELETE_REVIEW'
+// export const EDIT_REVIEW = 'EDIT_REVIEW'
+// export const GET_REVIEWS = 'GET_REVIEWS'
+// export const DELETE_REVIEW = 'DELETE_REVIEW'
 
+export const postReview = (productId, userId, rating, description) => (
+  dispatch
+) => {
+  axios
+    .post(`http://localhost:3001/reviews/${productId}/${userId}/`, {
+      rating,
+      description,
+    })
+    .then((review) => {
+      dispatch({
+        type: POST_REVIEW,
+        payload: review,
+      });
+    //   dispatch(getReviewsById(userId));
+    })
+    .catch((err) => console.log(err));
+};
 
-export const postReview = (reviewData) => {
-    return (dispatch) => {
+// export const editReview = (reviewData) => {
+//     return (dispatch) => {
+
+//         }
+// }
+
+// export const getReviews = (idProduct) => {
+//     return (dispatch) => {
         
-    }
-}
+//     }
+// }
 
-export const setReview = (review) => {
-    return (dispatch) => {
+// export const deleteReview = (data) => {
+//     return (dispatch) => {
 
-    }
-}
-
-export const editReview = (reviewData) => {
-    return (dispatch) => {
-
-        }
-}
-
-export const getReviews = (idProduct) => {
-    return (dispatch) => {
-        
-    }
-}
-
-export const deleteReview = (data) => {
-    return (dispatch) => {
-
-        }
-}
+//         }
+// }
--- client/src/redux/reviewsReducer/reducerReviews.js
+++ client/src/redux/reviewsReducer/reducerReviews.js
@@ -1,28 +1,18 @@
 import {
-POST_REVIEW, SET_REVIEW, EDIT_REVIEW, GET_REVIEWS, DELETE_REVIEW,} from './actionsReviews.js'
+POST_REVIEW, EDIT_REVIEW, GET_REVIEWS, DELETE_REVIEW,} from './actionsReviews.js'
 
 const initialState = {
-    review: {},
     reviews: []
 }
 
 const reducerReviews = (state = initialState, action) => {
-
-    switch (action.type) {
-      case POST_REVIEW:
-        return {};
-      case SET_REVIEW:
-        return {};
-      case EDIT_REVIEW:
-        return {};
-      case GET_REVIEWS:
-        return {};
-      case DELETE_REVIEW:
-        return {};
-
-      default:
-        return state;
+  if (action.type === POST_REVIEW) {
+    return {
+      ...state,
+      reviews: action.payload
     }
-}
+  }
+  return state;
+};
 
 export default reducerReviews;
