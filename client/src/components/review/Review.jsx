b742cf54043fb90818c5b6f3ca210d0c733174ec
fmarilao <facundomarilao@gmail.com>
2021-02-04 13:24:06 -0300

    other changes in reviews

--- client/src/components/review/Review.jsx
+++ client/src/components/review/Review.jsx
@@ -4,32 +4,33 @@ import { useSelector, useDispatch} from "react-redux";
 import {getReviews} from '../../redux/reviewsReducer/actionsReviews'
 import ReviewCard from "./ReviewCard";
 import { makeStyles } from '@material-ui/core/styles';
+import UserReviews from "./UserReview";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
+    direction:"row"
   },
   media: {
     height: 140,
   },
-});
+    padding: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(3)
+    },
+}))
 
 const Review = (props) => {
     const isLogged = useSelector((state) => state.loginReducer.isLogged);
     const allReviews = useSelector((state) => state.reviewsReducer.reviews)
     const dispatch = useDispatch()
-      const classes = useStyles();
+    const classes = useStyles();
     const {id} = props
     
     useEffect(() => {
         dispatch(getReviews(id))
     }, [])
-    
-    setTimeout(() => {
-        
-        console.log(allReviews);
-    }, 2000);
-    
+   
 
     // const reviewsMenu = () => {
     //     if (isLogged) {
@@ -57,33 +58,40 @@ const Review = (props) => {
         //     {reviewsMenu()}
         //     </h1>
         // </div>
-                <div>
+        <Grid container xs={12}>
+            <UserReviews />
+            <Grid item container xs={12}>
            {allReviews && allReviews.map(((review, index) =>{
                  return (
-                        <Card className={classes.root}>
-                        <CardActionArea>
-                            <CardMedia
-                            className={classes.media}
-                            image="/static/images/cards/contemplative-reptile.jpg"
-                            title="User Avatar"
-                            />
-                            <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                componente stars
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                Author: {review.author}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                {review.description}
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        </Card>
+                     <Grid className={classes.padding}>
+                        <Grid item container xs={12} >
+                                <Card>
+                                <CardActionArea>
+                                    <CardMedia
+                                    className={classes.media}
+                                    image="/static/images/cards/contemplative-reptile.jpg"
+                                    title="User Avatar"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        componente stars
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Author: {review.author}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {review.description}
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                </Card>
+                        </Grid>
+                        </Grid>
                     );
             }))
           }
-         </div>
+          </Grid>
+        </Grid>
     )
 }
 
--- client/src/components/review/UserReview.jsx
+++ client/src/components/review/UserReview.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import { Button, Divider, Grid, Input } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { postReview } from '../../redux/reviewsReducer/actionsReviews';
+import { useParams } from 'react-router-dom';
 
-export default function UserReviews() {
-  const [value, setValue] = React.useState(2);
+const UserReviews = () => {
+  const [description, setDescription] = useState('');
+  const [stars, setStars] = useState(3);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const userID = localStorage.getItem('userId');
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      dispatch(postReview());
+      //productId, userId, rating, description
+  };
 
   return (
     <div>
       <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Controlled</Typography>
+        <Typography component="legend">Send us your Review</Typography>
         <Rating
           name="simple-controlled"
-          value={value}
           onChange={(event, newValue) => {
-            setValue(newValue);
+            setStars(newValue);
           }}
         />
+        <Grid>
+                     <form onSubmit={handleSubmit}>
+                     <Input onChange={(e) => setDescription(e.target.value)} />
+                     <Divider />
+                     <Button variant="contained" color="primary" href="#contained-buttons">
+                        Send
+                      </Button>
+                     </form>
+ 
+            </Grid>
       </Box>
     </div>
   );
 }
+export default UserReviews
